1bcf98aacf241a6526e2fca860e5e8598c2325bb
istiamelia <istiamelia7@gmail.com>
2024-10-06 15:31:54 +0700

    Simplify the import keyword of variables in queries.ts by grouping them into one object

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,9 @@ import { Router, Request, Response } from "express";
 import { QueryResult } from "pg";
 import pool from "./config/db";
 import { v4 as uuidv4 } from 'uuid';
-import { getTasks, getTaskbyId, addTasks, deleteTask, updateTask, deleteAllTasks, updateCompleted, getCompletedTasks, getProjects } from "./queries";
+// import { getTasks, getTaskbyId, addTasks, deleteTask, updateTask, deleteAllTasks, updateCompleted, getCompletedTasks, getProjects } from "./queries";
+// Simplify the import keyword of variables in queries.ts by grouping them into one object
+import * as taskQueries from "./queries";
 
 const router = Router();
 export default router;
@@ -35,8 +37,8 @@ interface Project {
 // --- Get Request to fetch all data from the database
 router.get("/todos", async (req: Request, res: Response) => {
     try {
-        const result: QueryResult = await pool.query(getTasks);
-        const result2: QueryResult = await pool.query(getProjects);
+        const result: QueryResult = await pool.query(taskQueries.getTasks);
+        const result2: QueryResult = await pool.query(taskQueries.getProjects);
         const tasks: Task[] = result.rows;
         const projects: Project[] = result2.rows;
         res.render('index', { tasks, projects })
@@ -50,7 +52,7 @@ router.get("/todos", async (req: Request, res: Response) => {
 router.get("/todos/:task_id", async (req: Request, res: Response) => {
     const task_id = req.params['task_id']
     try {
-        const result: QueryResult = await pool.query(getTaskbyId(task_id));
+        const result: QueryResult = await pool.query(taskQueries.getTaskbyId(task_id));
         const todos: Task[] = result.rows;
         res.json(todos);
     } catch (error) {
@@ -65,7 +67,7 @@ router.post("/todos", async (req: Request, res: Response) => {
     const task_id = uuidv4()
     try {
         //create validation for the data type and the number of the characthers
-        await pool.query(addTasks, [task_id, task_name, task_description, task_asignee, task_status, task_priority, project_id, start_date, due_date]);
+        await pool.query(taskQueries.addTasks, [task_id, task_name, task_description, task_asignee, task_status, task_priority, project_id, start_date, due_date]);
         res.redirect('/todos')
     } catch (error) {
         console.error("Error saving todos", error);
@@ -78,11 +80,11 @@ router.delete("/todos/:task_id", async (req: Request, res: Response) => {
     const task_id = req.params['task_id']
     try {
         // first, checking wether the task exist in the database
-        const result: QueryResult = await pool.query(getTaskbyId(task_id));
+        const result: QueryResult = await pool.query(taskQueries.getTaskbyId(task_id));
         if (!result.rows.length) {
             res.send("Task does not exist in the database")
         }
-        await pool.query(deleteTask(task_id))
+        await pool.query(taskQueries.deleteTask(task_id))
         res.redirect('/todos')
     } catch (error) {
         console.error("Error deleting todos", error);
@@ -97,11 +99,11 @@ router.put("/todos/:task_id", async (req: Request, res: Response) => {
     const { task_name } = req.body;
     try {
         // first, checking wether the task exist in the database
-        const result: QueryResult = await pool.query(getTaskbyId(task_id));
+        const result: QueryResult = await pool.query(taskQueries.getTaskbyId(task_id));
         if (!result.rows.length) {
             res.send("Task does not exist in the database");
         };
-        await pool.query(updateTask(task_name, task_id.toString()));
+        await pool.query(taskQueries.updateTask(task_name, task_id.toString()));
         res.status(200).send("Task updated successfully!");
     } catch (error) {
         console.error("Error updating todos", error);
@@ -112,7 +114,7 @@ router.put("/todos/:task_id", async (req: Request, res: Response) => {
 // --- Delete Request to Delete all task
 router.delete("/todos", async (req: Request, res: Response) => {
     try {
-        await pool.query(deleteAllTasks)
+        await pool.query(taskQueries.deleteAllTasks)
         res.redirect('/todos')
     } catch (error) {
         console.error("Error deleting todos", error);
@@ -125,7 +127,7 @@ router.delete("/todos", async (req: Request, res: Response) => {
 router.post("/todos/:task_id", async (req: Request, res: Response) => {
     const task_id = req.params['task_id']
     try {
-        await pool.query(updateCompleted(task_id))
+        await pool.query(taskQueries.updateCompleted(task_id))
         res.redirect('/todos')
     } catch (error) {
         console.error("Error deleting todos", error);
@@ -139,7 +141,7 @@ router.post("/todos/:task_id", async (req: Request, res: Response) => {
 // --- after being rendered the content in completed.ejs will be injected into DOM via loadpartial() *see index.ts
 router.get('/path/to/completed', async (req: Request, res: Response) => {
     try {
-        const resultCompleted: QueryResult = await pool.query(getCompletedTasks);
+        const resultCompleted: QueryResult = await pool.query(taskQueries.getCompletedTasks);
         const completedTasks: Task[] = resultCompleted.rows;
         res.render('completed', { completedTasks })
     } catch (error) {
