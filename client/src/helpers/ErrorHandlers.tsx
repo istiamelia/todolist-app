266acc3f6b29f940be15d37d6808b3c65b370b88
istiamelia <istiamelia7@gmail.com>
2024-12-08 19:48:14 +0700

    Created a global error handler using Axios type checking and react-toastify for the error notification

---
+++ client/src/helpers/ErrorHandlers.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const handleError = (error: any) => {
+  if (axios.isAxiosError(error)) {
+    var err = error.response;
+    if (Array.isArray(err?.data.errors)) {
+      for (let val of err?.data.errors) {
+        toast.warning(val.description);
+      }
+    } else if (typeof err?.data.errors === "object") {
+      for (let e in err?.data.error) {
+        toast.warning(err?.data.error[e][0]);
+      }
+    } else if (err?.data) {
+      toast.warning(err.data);
+    } else if (err?.status === 401) {
+      //Since we're trying to create authorization this error handler will also check if the user directly access the main web page without trying to log in
+      toast.warning("Please login");
+      window.history.pushState({}, "LoginPage", "/login");
+      //pushState() method of the History Interface adds entry to the browser's session history stack. pushState()
+    } else if (err) {
+      toast.warning(err.data);
+    }
+  }
+};
